fd1f003679dcb4ae81e490d1a68429ea4306d12b
chanchalbhalla4 <chanchalbhalla4@gmail.com>
2017-07-14 10:42:26 +0530

    2 new menu choices are added.

--- insta.py
+++ insta.py
@@ -7,7 +7,7 @@ import urllib
 #Import termcolor library
 from termcolor import colored
 
-APP_ACCESS_TOKEN = '2291772577.01c7d34.2a5ae7597deb4347b8c0b029424a7d8d'
+APP_ACCESS_TOKEN = '2291772577.56784ea.bcb8eebedb9d4c998cfcdba93967e8fe'
 #Token Owner : chanchalbhalla4
 #Sandbox Users : friends usernames
 
@@ -27,9 +27,9 @@ def self_info():
             print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
             print 'No. of posts: %s' % (user_info['data']['counts']['media'])
         else:
-            print 'User does not exist!'
+            print colored('User does not exist!','red')
     else:
-        print 'Error due to Status code other than 200 received!'
+        print colored('Error due to Status code other than 200 received!','red')
 
 
 # Function for getting the ID of a user
@@ -96,7 +96,7 @@ def get_own_post():
 def get_user_post(insta_username):
     user_id = get_user_id(insta_username)
     if user_id == None:
-        print colored('User does not exist!','red')
+        print colored(' This User does not exist!','red')
         exit()
     request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
@@ -115,6 +115,48 @@ def get_user_post(insta_username):
 
 
 
+def get_post_id(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get(request_url).json()
+
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']):
+            return user_media['data'][0]['id']
+        else:
+            print 'There is no recent post of the user!'
+            exit()
+    else:
+        print 'Status code other than 200 received!'
+        exit()
+
+
+
+
+def get_like_list(insta_username):            # Defining the Function ............
+    media_id = get_post_id(insta_username)  # Getting post id by passing the username .......
+    request_url = BASE_URL + 'media/%s/likes?access_token=%s' % (media_id, APP_ACCESS_TOKEN)
+    print colored('GET request url : %s', 'blue') % (request_url)
+    like_list = requests.get(request_url).json()
+
+    if like_list['meta']['code'] == 200:  # checking the status code .....
+        if len(like_list['data']):
+            position = 1
+            print colored("List of people who Liked Your Recent post", 'blue')
+            for users in like_list['data']:
+                if users['username']!= None:
+                    print position, colored(users['username'],'green')
+                    position = position + 1
+                else:
+                    print colored('No one had d Your post!', 'red')
+        else:
+            print colored("User Does not have any post",'red')
+    else:
+        print colored('Status code other than 200 recieved', 'red')
 
 
 def start_bot():
@@ -126,24 +168,26 @@ def start_bot():
         print colored("2.) Get details of a user by username.", 'yellow')
         print colored("3.) Get your own recent post.",'yellow')
         print colored("4.) Get the recent post of a user by username.",'yellow')
-        print colored("5.) Exit", 'yellow')
+        print colored("5.) Get a list of people who have liked the recent post of a user", 'yellow')
+        print colored("6.) Exit", 'yellow')
 
         choice=raw_input(colored("Enter choice: ",'blue'))
         if choice=="1":
             self_info()
         elif choice=="2":
-            insta_username = raw_input(colored("Enter Username of the user: ", 'Green'))
+            insta_username = raw_input(colored("Enter Username of the user: ", 'green'))
             get_user_info(insta_username)
         elif choice == "3":
             get_own_post()
         elif choice == "4":
-            insta_username = raw_input(colored("Enter the username of the user: ",'Green'))
+            insta_username = raw_input(colored("Enter the username of the user: ",'green'))
             get_user_post(insta_username)
-
         elif choice=="5":
+            insta_username = raw_input("Enter the username of the user: ")
+            get_like_list(insta_username)
+        elif choice=="6":
             exit()
         else:
             print colored("You have entered a wrong choice", 'red')  #selected choice is wrong
 
 start_bot()
-
